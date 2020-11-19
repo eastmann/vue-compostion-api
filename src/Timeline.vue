51aa905de6be473b12df7ea88d73fb02d2f26f55
Maxim Klochkov <m.klochkov@sap.com>
2020-11-19 18:32:42 +0300

    Initial project setup

---
+++ src/Timeline.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="panel is-primary">
+        <p class="panel-tabs">
+            <a>Today</a>
+            <a>This week</a>
+            <a>This month</a>
+        </p>
+    </nav>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+const Timeline = defineComponent({
+
+})
+
+export default Timeline
+
+</script>
